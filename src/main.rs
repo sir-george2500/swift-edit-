2452a5b3f054e64627d265272ff242c90b57fe9c
George S. Mulbah <gsmulbah2500@gmail.com>
2024-04-29 19:56:41 +0000

    added code for printing one byte

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Read};
+
+fn main() -> io::Result<()> {
+    let mut c = [0; 1]; // Define a buffer to read a single byte at a time
+    loop {
+        match io::stdin().read_exact(&mut c) {
+            Ok(()) => continue, // Continue looping if exactly 1 byte is read successfully
+            Err(ref e) if e.kind() == io::ErrorKind::UnexpectedEof => break, // Break the loop if EOF is reached
+            Err(e) => return Err(e), // Propagate any other errors
+        }
+    }
+    Ok(())
 }
+

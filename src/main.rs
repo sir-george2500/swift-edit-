26e8f0e2a0a1491416cb30b519f77710dbd0b8ee
George S. Mulbah <gsmulbah2500@gmail.com>
2024-05-05 23:51:30 +0000

    refactor the main function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 /*** includes ***/
-use std::io::{self, Read};
-use libc::{ICRNL, IXON, OPOST};
-use termios::{tcsetattr, Termios, TCSAFLUSH};
-use std::os::unix::io::AsRawFd;
 use libc::c_int;
 use libc::iscntrl;
+use libc::{ICRNL, IXON, OPOST};
+use std::io::{self, ErrorKind, Read};
+use std::os::unix::io::AsRawFd;
+use termios::{tcsetattr, Termios, TCSAFLUSH};
+
+const CTRL_KEY_MASK: u8 = 0x1f;
+
+fn ctrl_key(c: u8) -> u8 {
+    c & CTRL_KEY_MASK
+}
 
 /*** data ***/
 
@@ -22,7 +28,9 @@ fn die(s: &str) -> ! {
 fn disable_raw_mode() -> Result<(), io::Error> {
     // Get the original terminal attributes from the global variable
     let orig_termios = unsafe {
-        ORIG_TERMIOS.take().ok_or_else(|| io::Error::new(io::ErrorKind::Other, "Original terminal attributes not set"))
+        ORIG_TERMIOS.take().ok_or_else(|| {
+            io::Error::new(io::ErrorKind::Other, "Original terminal attributes not set")
+        })
     }?;
     // Apply the original terminal attributes to the terminal
     if tcsetattr(io::stdin().as_raw_fd(), TCSAFLUSH, &orig_termios).is_err() {
@@ -52,7 +60,8 @@ fn enable_raw_mode() -> Result<(), io::Error> {
     raw.c_oflag &= !(OPOST);
     // Disable echoing of input characters and other miscellaneous flags
     raw.c_lflag &= !(termios::ECHO | termios::IEXTEN | termios::ICANON | termios::ISIG);
-    raw.c_iflag &= !(termios::BRKINT | termios::ICRNL | termios::INPCK | termios::ISTRIP | termios::IXON);
+    raw.c_iflag &=
+        !(termios::BRKINT | termios::ICRNL | termios::INPCK | termios::ISTRIP | termios::IXON);
     raw.c_cflag |= termios::CS8;
     raw.c_cc[termios::VMIN as usize] = 0;
     raw.c_cc[termios::VTIME as usize] = 1;
@@ -70,31 +79,42 @@ extern "C" fn disable_raw_mode_c() {
     }
 }
 
+/** Read Key **/
 /*** init ***/
-fn main() -> Result<(), io::Error> {
-    enable_raw_mode()?;
 
-    let mut input = [0u8; 1];
+fn editor_read_key() -> Result<char, io::Error> {
+    let mut buffer = [0u8; 1];
     loop {
-        // Use `read` instead of `read_exact`
-        if io::stdin().read(&mut input)? == 0 {
-            // Exit the loop if no bytes are read (EOF reached)
-            break;
-        }
-        let c = input[0];
-
-        if c == b'q' {
-            break;
-        }
-
-        // Use unsafe to call the C function iscntrl
-        if unsafe { iscntrl(c as c_int) } != 0 {
-            println!("{} (CTRL)\r\n", c);
-        } else {
-            println!("{} ('{}')\r\n", c, c as char);
+        match io::stdin().read_exact(&mut buffer) {
+            Ok(_) => {
+                return Ok(buffer[0] as char);
+            }
+            Err(ref e) if e.kind() == ErrorKind::WouldBlock => {}
+            Err(e) => {
+                return Err(e);
+            }
         }
     }
-
-    Ok(())
 }
 
+/**input **/
+
+fn editor_process_keypress() {
+    match editor_read_key() {
+        Ok(c) => {
+            if c as u8 == ctrl_key(b'q') {
+                // CTRL_KEY('q')
+                std::process::exit(0);
+            }
+        }
+        Err(e) => {
+            eprintln!("Error reading key: {}", e);
+        }
+    }
+}
+fn main() -> Result<(), io::Error> {
+    enable_raw_mode()?;
+    loop {
+        editor_process_keypress();
+    }
+}
